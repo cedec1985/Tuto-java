e99d90b0235e95223005fde2bf3d36a15ecfcb2c
cedec1985 <cedricdecraim@msn.com>
2023-10-16 16:04:28 +0200

    .......

--- exos/angular/phone-list.component.js
+++ exos/angular/phone-list.component.js
@@ -6,6 +6,14 @@ angular.
       '<span>{{phone.name}}</span>' +'<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
       '<p>{{phone.snippet}}</p>' +
     '</li>' +
+  '</ul>'+'<ul class="phones">'+
+  '<li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp" class="thumbnail">'+
+  '<a href="#!/phones/{{phone.id}}" class="thumb">'+
+  '<img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}" />'+
+  '</a>'+
+  '<a href="#!/phones/{{phone.id}}">{{phone.name}}</a>'+
+  '<p>{{phone.snippet}}</p>'+
+  '</li>'+
   '</ul>',
     controller: function PhoneListController() {
       this.phones = [
