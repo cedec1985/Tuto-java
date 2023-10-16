47aa8a3237c553126b87bf83b09d662f53b5c0b3
cedec1985 <cedricdecraim@msn.com>
2023-10-16 14:05:54 +0200

    .........

--- exos/angularjs/phone-list.component2.js
+++ exos/angularjs/phone-list.component2.js
@@ -1,18 +1,15 @@
 angular.
-  module('phonecatApp').
-  component('phoneList', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
-    template:   '<ul>' +
-    '<li ng-repeat="phone in $ctrl.phones">' +
-      '<span>{{phone.name}}</span>' +'<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
-      '<p>{{phone.snippet}}</p>' +
-    '</li>' +
-  '</ul>',
-    controller: function PhoneListController($http) {
+  module('phoneList').
+  component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: ['$http',
+      function PhoneListController($http) {
         var self = this;
         self.orderProp = 'age';
-  
-        $http.get('angularjs/phones.json').then(function(response) {
-            self.phones = response.data.slice(0, 5);
+
+        $http.get('phones/phones.json').then(function(response) {
+          self.phones = response.data;
         });
       }
-    });
+    ]
+  });
