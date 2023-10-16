370808e03d4f5b8053ac60e6bc624a8f8e93b85c
cedec1985 <cedricdecraim@msn.com>
2023-10-16 13:51:29 +0200

    .......

--- exos/angularjs/phone-list.component.js
+++ exos/angularjs/phone-list.component.js
@@ -1,9 +1,9 @@
 angular.
   module('phonecatApp').
   component('phoneList', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
-    template:   '<ul>' +
+    templateUrl:   '<ul>' +
     '<li ng-repeat="phone in $ctrl.phones">' +
-      '<span>{{phone.name}}</span>' +  '<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
+      '<span>{{phone.name}}</span>' +'<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
       '<p>{{phone.snippet}}</p>' +
     '</li>' +
   '</ul>',
@@ -11,14 +11,19 @@ angular.
       this.phones = [
         {
           name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
+          snippet: 'Fast just got faster with Nexus S.',
+          age: 1
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 2
         }, {
           name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
         }
       ];
+      this.orderProp = 'age';
     }
+
   });
