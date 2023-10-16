3e0cdc4c2b065d574af06d7dffb81a60f6641f3b
cedec1985 <cedricdecraim@msn.com>
2023-10-16 16:42:37 +0200

    ok

--- exos/angular/phone-list.component2.js
+++ exos/angular/phone-list.component2.js
@@ -6,13 +6,21 @@ angular.
       '<span>{{phone.name}}</span>' +'<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
       '<p>{{phone.snippet}}</p>' + 
     '</li>' +
+  '</ul>' +  '</ul>'+'<ul class="phones">'+
+  '<li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp" class="thumbnail">'+
+  '<a href="/#!/phones/{{phone.id}}" class="thumb">'+
+  '<img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}" />'+
+  '</a>'+
+  '<a href="/#!/phones/{{phone.id}}">{{phone.name}}</a>'+
+  '<p>{{phone.snippet}}</p>'+
+  '</li>'+
   '</ul>',
     controller: 
       function PhoneListController($http) {
         var self = this;
         self.orderProp = 'age';
 
-        $http.get('angularjs/phones.json').then(function(response) {
+        $http.get('/#!/phones.json').then(function(response) {
           self.phones = response.data.slice(0,5);
         });
     }
