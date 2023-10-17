2af99f042c972358a27426ef932e448c4d376221
cedec1985 <cedricdecraim@msn.com>
2023-10-17 09:10:45 +0200

    etc

--- exos/angular/phone-list.component.js
+++ exos/angular/phone-list.component.js
@@ -5,16 +5,16 @@ angular.
     '<li ng-repeat="phone in $ctrl.phones">' +
       '<span>{{phone.name}}</span>' +'<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
       '<p>{{phone.snippet}}</p>' +
-    '</li>' +
-  '</ul>'+'<ul class="phones">'+
-  '<li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp" class="thumbnail">'+
-  '<a href="/#/phones/{{phone.id}}" class="thumb">'+
-  '<img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}" />'+
-  '</a>'+
-  '<a href="/#/phones/{{phone.id}}">{{phone.name}}</a>'+
-  '<p>{{phone.snippet}}</p>'+
-  '</li>'+
-  '</ul>',
+    '</li>' +'<ul>'+
+    '</ul>'+'<ul class="phones">'+
+    '<li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp" class="thumbnail">'+
+    '<a href="/#/phones/{{phone.id}}" class="thumb">'+
+    '<img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}" />'+
+    '</a>'+
+    '<a href="/#/phones/{{phone.id}}">{{phone.name}}</a>'+
+    '<p>{{phone.snippet}}</p>'+
+    '</li>'+
+    '</ul>',
     controller: function PhoneListController() {
       this.phones = [
         {
--- exos/angular/phone-list.component2.js
+++ exos/angular/phone-list.component2.js
@@ -1,26 +1,13 @@
 angular.
   module('phoneList').
   component('phoneList', {
-    template:  '<ul>' +
-    '<li ng-repeat="phone in $ctrl.phones">' +
-      '<span>{{phone.name}}</span>' +'<p>Total number of phones: {{$ctrl.phones.length}}</p>' +
-      '<p>{{phone.snippet}}</p>' + 
-    '</li>' +
-  '</ul>' +  '</ul>'+'<ul class="phones">'+
-  '<li ng-repeat="phone in $ctrl.phones | filter:$ctrl.query | orderBy:$ctrl.orderProp" class="thumbnail">'+
-  '<a href="/#!/phones/{{phone.id}}" class="thumb">'+
-  '<img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}" />'+
-  '</a>'+
-  '<a href="/#!/phones/{{phone.id}}">{{phone.name}}</a>'+
-  '<p>{{phone.snippet}}</p>'+
-  '</li>'+
-  '</ul>',
+    templateUrl : 'angular/phone-list.template.html',
     controller: 
       function PhoneListController($http) {
         var self = this;
         self.orderProp = 'age';
 
-        $http.get('http://127.0.0.1:5500/phones.json').then(function(response) {
+        $http.get('phones.json').then(function(response) {
           self.phones = response.data.slice(0,5);
         });
     }
