92d8245316cdb700f5a7cc79b046768f4b4cea49
cedec1985 <cedricdecraim@msn.com>
2023-10-17 09:43:05 +0200

    ok

--- exos/angular/phone-list.component2.js
+++ exos/angular/phone-list.component2.js
@@ -1,14 +1,10 @@
 angular.
   module('phoneList').
   component('phoneList', {
-    templateUrl : 'angular/phone-list.template.html',
-    controller: 
-      function PhoneListController($http) {
-        var self = this;
-        self.orderProp = 'age';
-
-        $http.get('phones.json').then(function(response) {
-          self.phones = response.data.slice(0,5);
-        });
-    }
-  });
+    templateUrl : 'exos/angular/phone-list.template.html',
+    controller: ['Phone',
+      function PhoneListController(Phone) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+        }]
+    });
